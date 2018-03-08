6e823e97c2fd66d87c8bc8c6aaa20a0990c0e6af
jfgout <jfgout@gmail.com>
2018-03-07 18:13:11 -0700

    Update example.R

--- example.R
+++ example.R
@@ -1,4 +1,7 @@
+# Example 1: Regular parallax measurement.
 # Calculating the distance from Earth to asteroid (3200) Phaethon from two images taken from Scottsdale (Arizona, USA) and Antibes (France) at the same time.
+
+source("parallax.R")
 
 # Geographical coordinates for the two observing sites:
 A = c(33.469543, -111.934369) #Scottsdale
@@ -15,3 +18,25 @@ parallax = 153/(60*60)
 getDistanceToTarget(A, B, parallax, ALT, AZ)
 
 # Resut: 10.63 million kilometers (true value was 10.58 million km according to Stellarium... not bad!)
+
+#--------------------------
+
+# Example 2: Distance to Tiangong-1 during a lunar transit.
+
+source("parallax.R")
+
+alpha = 252 # Angular distance between the two parallel lines corresponding to the satellite passing in front of the Moon/Sun from 2 nearby positions
+
+A = c( getDecimalCoords(33,30,03.07), 0-getDecimalCoords(111,37,04.75) )	# Coordinates for observer #1
+B = c( getDecimalCoords(33,30,16.60), 0-getDecimalCoords(111,37,02.45) )	# Coordinates for observer #2
+
+L1 = c( getDecimalCoords(33,30,19.68), 0-getDecimalCoords(111,37,48.64) ) # Coordinates of a random point on the line of centrality (choose a point within a few miles of observing site)
+L2 = c( getDecimalCoords(33,29,29.74), 0-getDecimalCoords(111,36,10.43) ) # Coordinates of a second random point on the line of centrality (choose a point within a few miles of observing site and a few miles from point L1)
+
+# Position of the Moon/Sun at the time of transit
+ALT = 55.0
+AZ = 146.6
+
+parallax = alpha/(60*60)
+
+getDistanceToTarget(A, B, parallax, ALT, AZ, L1, L2)
